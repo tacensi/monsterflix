b5631f417e95e6ffa051d7af599a5b85f3fbc8d5
Thiago Censi <thiago.censi@pebmed.com.br>
2020-07-28 23:11:59 -0300

    home ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import dados_iniciais from './data/dados_iniciais.json';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
+  const categorias = dados_iniciais.categorias;
+  const categoriasComponents = [];
+  categorias.forEach((cat, index) => {
+    const ignore = index === 0 ? 'ignoreFirstVideo' : null;
+    categoriasComponents.push(
+      <Carousel
+        ignoreFirstVideo={ignore}
+        category={cat}
+      />
+    );
+  });
+
   return (
-    <div>
+    <div style={{ background: '#141414' }}>
       <Menu />
+
+      <BannerMain
+        videoTitle={dados_iniciais.categorias[0].videos[0].titulo}
+        url={dados_iniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área"}
+      />
+
+      {categoriasComponents}
+
+      <Footer />
     </div>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
-import ButtonLink from '../ButtonLink';
+import Button from '../Button';
 
 function Menu() {
     return (
-        <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="Logo da MonsterFlix" />
-            </a>
-            <ButtonLink className="ButtonLink" href="/">
-                Novo Vídeo
-            </ButtonLink>
-        </nav>
-    )
+      <nav className="Menu">
+        <a href="/">
+          <img className="Logo" src={Logo} alt="Logo da MonsterFlix" />
+        </a>
+        <Button as="a" className="ButtonClick" href="/">
+          Novo Vídeo
+        </Button>
+      </nav>
+    );
 }
 
 export default Menu;
