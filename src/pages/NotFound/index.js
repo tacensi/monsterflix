7ee481119a6feacaf9baca14f403b59945adc726
Thiago Censi <thiago.censi@pebmed.com.br>
2020-07-29 23:07:29 -0300

    on a diet

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,17 +1,40 @@
 import React from "react";
-import Travolta from "../../assets/img/Travolta.gif";
+import Travolta from "../../assets/img/Travolta.png";
 import PageDefault from "../../components/PageDefault";
+import styled from 'styled-components';
 
-function CadastroVideo() {
+const backgroundImage = () => {
+  let imgs = [
+    "https://dicasnovayork.com.br/wp-content/uploads/2017/03/hospedagem_header-1000x700.jpg",
+    "https://i.pinimg.com/originals/1a/79/3b/1a793b05229e9e1e6a5391783c97bade.jpg",
+    "https://s.france24.com/media/display/a6391d38-08f5-11e9-a9fd-005056a964fe/w:1280/p:16x9/260416-champs-elysees-m.webp",
+  ];
+
+  return imgs[Math.floor(Math.random() * imgs.length)];
+};
+
+const Main = styled.div`
+  background-size: cover;
+  background-position: bottom;
+  background-repeat: no-repeat;
+  background-image: url(${backgroundImage});
+
+  img {
+      display: block;
+  }
+`;
+
+
+function NotFound() {
   return (
     <PageDefault>
       <h1>404 - Conteúdo não encontrado</h1>
 
-      <div className="notFound">
+      <Main>
         <img src={Travolta} alt="where da fuq is this" />
-      </div>
+      </Main>
     </PageDefault>
   );
 }
 
-export default CadastroVideo;
+export default NotFound;
